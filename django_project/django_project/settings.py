31a7b63b7cebb240ced9d4f47206d078f0e77704
Leo Locatelli <leolocatelli30@gmail.com>
2024-11-16 20:54:58 +0100

    gunicord2

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -94,6 +94,7 @@ USE_TZ = True
 # Static and Media Files
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"  # For production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
