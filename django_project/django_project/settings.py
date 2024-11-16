23eee4427db3067822e3534bc02ff2f990b6f88e
Leo Locatelli <leolocatelli30@gmail.com>
2024-11-16 21:30:19 +0100

    staticfiles

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -13,6 +13,7 @@ DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 # Allowed Hosts
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split()
 ALLOWED_HOSTS = ['ucd-assignment-5-django-blog-5.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Installed Apps
